10176b61ef953f421a40f1eb36e0fb565566a0b8
Li <1925060001@henu.edu.cn>
2021-03-18 16:39:55 +0800

    删去了不需要的代码

--- App.js
+++ App.js
@@ -199,73 +199,13 @@ export default class home extends Component{
               }}
               data={this.chartDates}
             />
-
           </VictoryChart>: <Text> </Text> }
-          
-    
-
-       { /* <VictoryChart
-            width={550}
-            height={90}
-            scale={{x: "time"}}
-            padding={{top: 0, left: 50, right: 50, bottom: 30}}
-            containerComponent={
-              <VictoryBrushContainer responsive={false}
-                brushDimension="x"
-                brushDomain={this.state.selectedDomain}
-                onBrushDomainChange={this.handleBrush.bind(this)}
-              />
-            }
-          >
-            <VictoryAxis
-              tickValues={[
-                new Date(1985, 1, 1),
-                new Date(1990, 1, 1),
-                new Date(1995, 1, 1),
-                new Date(2000, 1, 1),
-                new Date(2005, 1, 1),
-                new Date(2010, 1, 1),
-                new Date(2015, 1, 1)
-              ]}
-              tickFormat={(x) => new Date(x).getFullYear()}
-            />
-            <VictoryLine
-              style={{
-                data: {stroke: "tomato"}
-              }}
-              data={[
-                {x: new Date(1982, 1, 1), y: 125},
-                {x: new Date(1987, 1, 1), y: 257},
-                {x: new Date(1993, 1, 1), y: 345},
-                {x: new Date(1997, 1, 1), y: 515},
-                {x: new Date(2001, 1, 1), y: 132},
-                {x: new Date(2005, 1, 1), y: 305},
-                {x: new Date(2011, 1, 1), y: 270},
-                {x: new Date(2015, 1, 1), y: 470}
-              ]}
-            />
-            </VictoryChart>*/}
-      
-
-
-         
-          </View>
-          
+          </View>         
         </View>
       );
     }
 }
 
-const chartConfig = {
-  backgroundGradientFrom: "#1E2923",
-  backgroundGradientFromOpacity: 0,
-  backgroundGradientTo: "#08130D",
-  backgroundGradientToOpacity: 0.5,
-  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-  strokeWidth: 2, // optional, default 3
-  barPercentage: 0.5,
-  useShadowColorFromDataset: false // optional
-};
 const styles=StyleSheet.create({
     serch:{
       flexDirection:'row',
