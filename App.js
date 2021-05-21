90bc4b5c913e8dc0d6b31c6517bcd364fe61fc2c
李永晖 <1925060001@henu.edu.cn>
2021-05-21 16:31:16 +0800

    Revert "图表横坐标改为时间"
    This reverts commit 90a5e86f0ba34dedc2e50d450ada88b73838511b

--- App.js
+++ App.js
@@ -242,9 +242,8 @@ export default class home extends Component{
         value2.end=t2;
         value2.time=value2.end-value2.begin;
         if(value2.time!=0){
-          let xtime=`${new Date().getHours()}:`+`${new Date().getMinutes()}:`+`${new Date().getSeconds()}:`+`${new Date().getMilliseconds()}`
-        const data={y:value2.time,x:xtime};
-        if(this.linechartDates2.length>4){
+        const data={y:value2.time,x:x2};
+        if(this.linechartDates2.length>130){
           this.linechartDates2.shift();
         }
         this.linechartDates2.push(data);
@@ -301,9 +300,8 @@ export default class home extends Component{
         value.end=t2;
         value.time=value.end-value.begin;
         if(value.time!=0){
-        let xtime=`${new Date().getHours()}:`+`${new Date().getMinutes()}:`+`${new Date().getSeconds()}:`+`${new Date().getMilliseconds()}`
-        const data={y:value.time,x:xtime};
-        if(this.linechartDates.length>4){
+        const data={y:value.time,x:x};
+        if(this.linechartDates.length>130){
           this.linechartDates.shift();
         }
         this.linechartDates.push(data);
