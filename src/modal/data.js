13511f1bf67ee9d38be01333891ae7a44c79a843
liaorongtao <1925060196@henu.edu.cn>
2021-07-27 22:26:07 +0800

    使用Icon图标，优化一部分页面

--- src/modal/data.js
+++ src/modal/data.js
@@ -1,8 +1,5 @@
 const data = {
     local: [], // 用户输入历史
     userChoose: '', // 用户语言设置选择
-    language: ['zh', 'en'],
-    // 展示用户选择的数据
-    languageshow: ['简体中文(中国)', '英语(美国)'],
 };
 export default data;
